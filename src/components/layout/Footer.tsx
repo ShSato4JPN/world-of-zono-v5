f429d763e74f1df83c68e0d2567a1bf144910153
ShSato4JPN <shsato4.private.1221@gmail.com>
2026-02-12 22:17:06 +0900

    feat: フッターに copyright を追加

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,3 +1,15 @@
+import dayjs from "dayjs";
+
 export default function Footer() {
-  return <div className="bg-blue-200">Footer</div>;
+  const currentYear = dayjs().year();
+
+  return (
+    <footer>
+      <div className="mx-auto max-w-6xl px-4 py-6 sm:px-6">
+        <p className="text-center text-xs md:text-sm text-zinc-500 dark:text-zinc-400">
+          &copy; {currentYear} World of Zono. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
 }
